16eb5079c238518643413626e42743a4d9840fa8
Rox523 <www.feart39@gmail.com>
2024-07-28 20:37:43 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+immutable_var = 'apple', 1 , 2, 'work'
+#immutable_var[2] = 7 # мы не можем изменить так как не являеться списмком
+print(immutable_var)
+mutable_list = ([1,2,3,4 ,"day",'night'])
+print(mutable_list)
+mutable_list.append(True)
+print(mutable_list)
+mutable_list[0] = 7
+print(mutable_list)
+
+
+
+
+
